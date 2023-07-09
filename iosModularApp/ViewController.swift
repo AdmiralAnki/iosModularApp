45487f08caf5c3c4c3ae5d44b440226b0351f377
ankith <ankithkirti@gmail.com>
2023-07-09 21:45:06 +0530

    Initial Commit

---
+++ iosModularApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosModularApp
+//
+//  Created by Ankith 2 on 09/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
